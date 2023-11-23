b88273576958df2244a54824e6e1d0d32d097fc4
Vijay Sai Krishnamoorthy <vijaysai@meta.com>
2023-11-22 20:01:03 -0800

    ui changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>AI Chatbot</h1>
+    <div>
+      <input type="password" v-model="apiKey" placeholder="Enter OpenAI API Key" />
+      <button @click="saveApiKey">Save API Key</button>
+    </div>
     <div class="chat-window">
       <ul>
         <li v-for="message in messages" :key="message.id" :class="{'user-message': message.type === 'user', 'ai-message': message.type === 'ai'}">
@@ -8,38 +12,64 @@
         </li>
       </ul>
     </div>
-    <input type="text" v-model="userInput" @keyup.enter="sendMessage"/>
+    <input type="text" v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message here"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'App',
   data() {
     return {
+      apiKey: '',
       userInput: '',
       messages: [],
       nextId: 0
-    }
+    };
   },
   methods: {
+    addMessage(type, text) {
+      this.messages.push({ id: this.nextId++, type, text });
+    },
+    async fetchAIResponse(prompt) {
+      if (!this.apiKey.trim()) {
+        alert('API key is required');
+        return;
+      }
+
+      try {
+        const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+          prompt: prompt,
+          max_tokens: 150
+        }, {
+          headers: {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/json'
+          }
+        });
+
+        this.addMessage('ai', response.data.choices[0].text.trim());
+      } catch (error) {
+        console.error('Error fetching AI response:', error);
+        alert('Failed to fetch AI response. Check the console for more details.');
+      }
+    },
     sendMessage() {
       if (!this.userInput.trim()) return;
 
-      // Add user message
       this.addMessage('user', this.userInput);
 
-      // Send the user input to the AI backend and get the response
-      // For now, we'll just mock this
-      setTimeout(() => {
-        this.addMessage('ai', `AI Response to: ${this.userInput}`);
-      }, 1000);
+      this.fetchAIResponse(this.userInput);
 
-      // Clear the user input
       this.userInput = '';
     },
-    addMessage(type, text) {
-      this.messages.push({ id: this.nextId++, type: type, text: text });
+    saveApiKey() {
+      if (!this.apiKey.trim()) {
+        alert('Please enter a valid API key');
+        return;
+      }
+      alert('API Key saved! You can now interact with the AI.');
     }
   }
 }
@@ -57,6 +87,7 @@ export default {
   height: 300px;
   overflow-y: auto;
   margin-bottom: 20px;
+  background-color: #f9f9f9;
 }
 ul {
   list-style-type: none;
@@ -74,9 +105,14 @@ li {
   color: green;
   text-align: left;
 }
-input[type="text"] {
-  width: 100%;
+input[type="text"], input[type="password"] {
+  width: 70%;
   padding: 10px;
+  margin-bottom: 10px;
+}
+button {
+  padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
 
